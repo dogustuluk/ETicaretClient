f7c531e2b4700e1db836d0489b7cd9fcd30dc10e
dogustuluk <dogus.tuluk@gmail.com>
2024-06-05 12:47:01 +0300

    signalR

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { AlertifyService,MessageType,Position } from 'src/app/services/admin/alertify.service';
+import { HubUrls } from '../../../constants/hub-urls';
+import { ReceiveFunctions } from '../../../constants/receive-functions';
+import { SignalRService } from '../../../services/common/signalr.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,10 +12,17 @@ import { AlertifyService,MessageType,Position } from 'src/app/services/admin/ale
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent extends BaseComponent implements OnInit {
-  constructor(private alertify : AlertifyService, spinner : NgxSpinnerService){
+  constructor(private alertify: AlertifyService, spinner: NgxSpinnerService, private signalRService: SignalRService) {
     super(spinner);
+    signalRService.start(HubUrls.ProductHub);
   }
   ngOnInit(): void {
+    this.signalRService.on(ReceiveFunctions.ProductAddedMessageReceiveFunction, message => {
+      this.alertify.message(message, {
+        messageType: MessageType.Notify,
+        position: Position.TopRight
+      });
+    });
   }
 
   m(){
---
+++ src/app/services/common/signalr.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { HubConnection, HubConnectionBuilder, HubConnectionState } from '@microsoft/signalr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignalRService {
+
+  private _connection: HubConnection;
+  get connection(): HubConnection {
+    return this._connection;
+  }
+
+  start(hubUrl: string) {
+    //başlatılmış bir hub dönecek.
+    if (!this.connection || this.connection?.state == HubConnectionState.Disconnected) {
+      //hub oluşturacak olan builder
+      const builder: HubConnectionBuilder = new HubConnectionBuilder();
+      //builder üzerinden hub connection oluştur. withAutomaticReconnect ile otomatik bağlar.
+      const hubConnection: HubConnection =
+        builder.withUrl(hubUrl)
+          .withAutomaticReconnect()
+          .build();
+
+      //connection'ı başlatırız
+      hubConnection.start()
+        .then(() => console.log("Connected"))
+        //bağlantıda sorun varsa her 2 saniyede bir tetikleme yaparız
+        .catch(error => setTimeout(() => this.start(hubUrl), 2000));
+      this._connection = hubConnection;
+
+    }
+
+    //bağlantı kurulduktan sonra bağlantı kopacak veya kurulma süreci olacak veya bağlantıyı tekrardan kurmaya çalışırken istemeden bağlantı kopacak. bu durumları yönetmek için ->
+    this._connection.onreconnected(connectionId => console.log("Reconnected"));//connectionId'yi bize sağlar. istenilen işlemi yaparız.
+    this._connection.onreconnecting(error => console.log("Reconnecting"))//kopan bağlantının tekrardan bağlanma sürecinde olduğunu ifade eder.
+    this._connection.onclose(error => console.log("Close Reconnection"));//tekrar edilen bağlantının kurulamadığını söyler.
+    //---
+
+
+  }
+
+  invoke(procedureName: string, message: any, successCallBack?: (value) => void, errorCallBack?: (error) => void) {
+    //signalR üzerinden herhangi bir client'ın bir başka client'a mesaj göndermek istersek. event fırlatmak gibi.
+    this.connection.invoke(procedureName, message)
+      .then(successCallBack)
+      .catch(errorCallBack);
+  }
+
+  ///'...' yaptığımızda c#'taki params'a karşılık gelir, bir dizi olarak tanımlar
+  on(procedureName: string, callBack: (...message: any) => void) {
+    //server'dan gelecek mesajların runtime'da anlık olarak yakalanmasını sağlar. temel alıcı fonksiyonları tanımlarız. gelen mesaja karşılık hangi event'in tetikleneceğini yazarız.
+    this.connection.on(procedureName, callBack);
+  }
+}
